91b7a0746a0a485f7d1ddf645163bc5cc3da7f08
Jeevan2005s <jeevanprasad2005s@gmail.com>
2025-02-18 19:13:00 +0530

    2nd commit

---
+++ rem.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class rem{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String l1=s.nextLine();
+        String l2=s.nextLine();
+        String[] list2=l2.split(" ");
+        for(int i=0;i<=list2.length;i++){
+            list1.add(list2[i]);
+        }
+    }
+}
